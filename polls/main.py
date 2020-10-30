fc84644218ffa7bae85c9e0fb9fc869101480415
Abhishek Juneja <abhishek.juneja145@gmail.com>
2020-10-30 19:00:31 +0100

    setup jinja2, list view template, detail view template, ORM queries, record not found error

--- polls/main.py
+++ polls/main.py
@@ -1,12 +1,19 @@
+import aiohttp_jinja2
+import jinja2
 from aiohttp import web
 from db import close_pg, init_pg
 from routes import setup_routes
-from settings import config
+from settings import BASE_DIR, config
 
 app = web.Application()
+app["config"] = config
+
+aiohttp_jinja2.setup(
+    app, loader=jinja2.FileSystemLoader(str(BASE_DIR / "polls" / "templates"))
+)
 
 setup_routes(app)
 app.on_startup.append(init_pg)
 app.on_cleanup.append(close_pg)
-app["config"] = config
+
 web.run_app(app)
