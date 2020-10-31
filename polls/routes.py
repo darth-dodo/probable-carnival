0367be54f67da0a6a497fbe77babb3fa64f99c50
Abhishek Juneja <darth-dodo@users.noreply.github.com>
2020-10-31 22:21:45 +0100

    Poll Results (#7)

--- polls/routes.py
+++ polls/routes.py
@@ -1,6 +1,6 @@
 import pathlib
 
-from views import index, poll
+from views import index, poll, results
 
 PROJECT_ROOT = pathlib.Path(__file__).parent
 
@@ -8,6 +8,7 @@ PROJECT_ROOT = pathlib.Path(__file__).parent
 def setup_routes(app):
     app.router.add_get("/", index)
     app.router.add_get("/poll/{question_id}", poll, name="poll")
+    app.router.add_get("/poll/{question_id}/results", results, name="results")
 
     setup_static_routes(app)
 
