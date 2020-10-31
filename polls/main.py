b477b499b411e51df1c063f629f5ea2de60d6063
Abhishek Juneja <darth-dodo@users.noreply.github.com>
2020-10-31 22:00:48 +0100

    Exception Middlewares (#5)

--- polls/main.py
+++ polls/main.py
@@ -2,6 +2,7 @@ import aiohttp_jinja2
 import jinja2
 from aiohttp import web
 from db import close_pg, init_pg
+from middlewares import setup_middlewares
 from routes import setup_routes
 from settings import BASE_DIR, config
 
@@ -13,6 +14,7 @@ aiohttp_jinja2.setup(
 )
 
 setup_routes(app)
+setup_middlewares(app)
 app.on_startup.append(init_pg)
 app.on_cleanup.append(close_pg)
 
